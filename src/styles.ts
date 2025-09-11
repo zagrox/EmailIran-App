69eededeb942961151c75c137ee934a82da288e4
ZAGROX <37931784+zagrox@users.noreply.github.com>
2025-09-11 19:29:28 +0330

    refactor: Remove deprecated styles file
    The styles.ts file is no longer used as all styling has been moved to the index.html file using Tailwind CSS. This commit removes the obsolete file to clean up the project.

--- src/styles.ts
+++
@@ -1,4 +0,0 @@
-// This file is deprecated. All styles have been centralized
-// into the <style type="text/tailwindcss"> block in index.html.
-// This file can be safely deleted from the project.
-export const STYLES = {};
